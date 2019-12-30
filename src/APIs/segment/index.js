bfcea8639e3d2d119bba418e5cecbec563c9bdeb
James McManamey <jfmcmanamey@gmail.com>
2019-12-30 13:20:27 -0800

    moving segment key to env variable

--- src/APIs/segment/index.js
+++ src/APIs/segment/index.js
@@ -1,5 +1,7 @@
-var Analytics = require("analytics-node");
-var analytics = new Analytics("Rm3ZRvruxNDrNI56RQ5DNiyylyO1Gnny", {
+const Analytics = require("analytics-node");
+const { SEGMENT_WRITE_KEY, LOG_LEVEL } = require("../../../config");
+
+module.exports =  new Analytics(SEGMENT_WRITE_KEY, {
   flushAt: 1
 });
-module.exports = analytics;
+
